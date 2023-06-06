a9e6130af98d84dc67e293994675d064628fcdfa
Isaac Fierro <zifg_36@live.com.mx>
2023-06-06 10:03:05 -0700

    Nuevo template agregado

---
+++ templates/static/LogoutView-39c7595b.js
@@ -0,0 +1,1 @@
+import{am as a,an as o}from"./index-98dfde43.js";const c={__name:"LogoutView",setup(r){const t=a(),e=o();return localStorage.clear(),e.reiniciarDatosAuth(),t.push({name:"auth-login"}),(u,s)=>null}};export{c as default};
